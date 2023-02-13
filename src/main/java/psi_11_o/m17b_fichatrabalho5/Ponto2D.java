fd8b9dfaf30be0a3ff194728d06d0cc7b670d42a
Alexandru Racicovschi <>
2023-02-13 16:04:55 +0000

    Criação de comentarios Javadoc ficha de trtabalho 5.

--- src/main/java/psi_11_o/m17b_fichatrabalho5/Ponto2D.java
+++ src/main/java/psi_11_o/m17b_fichatrabalho5/Ponto2D.java
@@ -15,11 +15,39 @@ public class Ponto2D {
     }
     
     public double distancia(Ponto2D a){
-        return Math.abs(Math.sqrt(Math.pow((x-a.x),2)+Math.pow((y-a.y),2)));
+        return Math.abs(Math.sqrt(Math.pow((getX()-a.getX()),2)+Math.pow((getY()-a.getY()),2)));
     }
 
     @Override
     public String toString() {
-        return "Ponto2D[" + x + "," + y + "]";
+        return "Ponto2D[" + getX() + "," + getY() + "]";
     }    
+
+    /**
+     * @return mostra o valor do x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x muda o valor do x
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return mostra o valor do y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y muda o valor do y
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
 }
